fba305589231f0b3fd0ff7759fb111e1a9bb8e11
Emiliano Sotomayor Gonzalez <uboat46@hotmail.com>
2017-04-19 20:52:03 -0500

    added events

--- RadioCRTV/js/main.js
+++ RadioCRTV/js/main.js
@@ -1,84 +1,3 @@
-//   document.getElementById("ModuloUno").style.display = 'none';
-//   document.getElementById("ModuloDos").style.display = 'none';
-//   document.getElementById("ModuloTres").style.display = 'none';
-
-//   document.getElementById("ModuloUnoDos").style.display = 'none';
-//   document.getElementById("ModuloDosDos").style.display = 'none';
-//   document.getElementById("ModuloTresDos").style.display = 'none';
-
-//   document.getElementById("ModuloUnoTres").style.display = 'none';
-//   document.getElementById("ModuloDosTres").style.display = 'none';
-//   document.getElementById("ModuloTresTres").style.display = 'none';
-
-//   var x =document.getElementById("ModuloPrincipalBoton");
-//   var y =document.getElementById("ModuloUnoBoton");
-//   var z =document.getElementById("ModuloDosBoton");
-//   var w =document.getElementById("ModuloTresBoton");
-//   x.onclick = function() {modulos(0)};
-//   y.onclick = function() {modulos(1)};
-//   z.onclick = function() {modulos(2)};
-//   w.onclick = function() {modulos(3)};
-
-// function modulos(i){
-//   switch (i) {
-//     case 0:
-//         document.getElementById("ModuloPrincipal").style.display = 'block';
-//         document.getElementById("ModuloUno").style.display = 'none';
-//         document.getElementById("ModuloDos").style.display = 'none';
-//         document.getElementById("ModuloTres").style.display = 'none';
-
-//         document.getElementById("ModuloUnoDos").style.display = 'none';
-//         document.getElementById("ModuloDosDos").style.display = 'none';
-//         document.getElementById("ModuloTresDos").style.display = 'none';
-
-//         document.getElementById("ModuloUnoTres").style.display = 'none';
-//         document.getElementById("ModuloDosTres").style.display = 'none';
-//         document.getElementById("ModuloTresTres").style.display = 'none';
-
-//         break;
-//     case 1:
-//           document.getElementById("ModuloPrincipal").style.display = 'none';
-//           document.getElementById("ModuloUno").style.display = 'block';
-//           document.getElementById("ModuloDos").style.display = 'block';
-//           document.getElementById("ModuloTres").style.display = 'block';
-
-//           document.getElementById("ModuloUnoDos").style.display = 'none';
-//           document.getElementById("ModuloDosDos").style.display = 'none';
-//           document.getElementById("ModuloTresDos").style.display = 'none';
-
-//           document.getElementById("ModuloUnoTres").style.display = 'none';
-//           document.getElementById("ModuloDosTres").style.display = 'none';
-//           document.getElementById("ModuloTresTres").style.display = 'none';
-//         break;
-//     case 2:
-//           document.getElementById("ModuloPrincipal").style.display = 'none';
-//           document.getElementById("ModuloUno").style.display = 'none';
-//           document.getElementById("ModuloDos").style.display = 'none';
-//           document.getElementById("ModuloTres").style.display = 'none';
-
-//           document.getElementById("ModuloUnoDos").style.display = 'block';
-//           document.getElementById("ModuloDosDos").style.display = 'block';
-//           document.getElementById("ModuloTresDos").style.display = 'block';
-
-//           document.getElementById("ModuloUnoTres").style.display = 'none';
-//           document.getElementById("ModuloDosTres").style.display = 'none';
-//           document.getElementById("ModuloTresTres").style.display = 'none';
-//         break;
-//     case 3:          
-//     document.getElementById("ModuloPrincipal").style.display = 'none';
-//     document.getElementById("ModuloUno").style.display = 'none';
-//     document.getElementById("ModuloDos").style.display = 'none';
-//     document.getElementById("ModuloTres").style.display = 'none';
-
-//     document.getElementById("ModuloUnoDos").style.display = 'none';
-//     document.getElementById("ModuloDosDos").style.display = 'none';
-//     document.getElementById("ModuloTresDos").style.display = 'none';
-
-//     document.getElementById("ModuloUnoTres").style.display = 'block';
-//     document.getElementById("ModuloDosTres").style.display = 'block';
-//     document.getElementById("ModuloTresTres").style.display = 'block';
-//   }
-// }
 var slideIndex = 1;
 showDivs();
 
@@ -148,11 +67,15 @@ function showDivs() {
     {
       document.getElementById("player").classList.remove("display-off");
       document.getElementById("player").classList.add("display-on");
+      document.getElementById("livee").style.color = "red";
+      document.getElementById("live2").style.color = "red";
     }
     else
     {
       document.getElementById("player").classList.remove("display-on");
       document.getElementById("player").classList.add("display-off");
+      document.getElementById("livee").style.color = "#aba4a4";
+      document.getElementById("live2").style.color = "#aba4a4";
     }
   });
 
@@ -161,12 +84,85 @@ function showDivs() {
     {
       document.getElementById("player").classList.remove("display-off");
       document.getElementById("player").classList.add("display-on");
+      document.getElementById("livee").style.color = "red";
+      document.getElementById("live2").style.color = "red";
     }
     else
     {
       document.getElementById("player").classList.remove("display-on");
       document.getElementById("player").classList.add("display-off");
+      document.getElementById("livee").style.color = "#aba4a4";
+      document.getElementById("live2").style.color = "#aba4a4";
     }
   });
 // END Open event of player //===============
 //=================================================
+
+// Change event for programas =====================
+  document.getElementById("programas").addEventListener("click",function(){
+    if(document.getElementById("radio").classList.contains("display-off"))
+    {
+      document.getElementById("radio").classList.remove("display-off");
+      document.getElementById("radio").classList.add("display-on");
+      document.getElementById("carrousel").classList.remove("display-on");
+      document.getElementById("carrousel").classList.add("display-off");
+    }
+  });
+
+  document.getElementById("programas2").addEventListener("click",function(){
+    if(document.getElementById("radio").classList.contains("display-off"))
+    {
+      document.getElementById("radio").classList.remove("display-off");
+      document.getElementById("radio").classList.add("display-on");
+      document.getElementById("carrousel").classList.remove("display-on");
+      document.getElementById("carrousel").classList.add("display-off");
+    }
+  });
+// END Open event of player //===============
+//=================================================
+
+// Change event for main =====================
+  document.getElementById("header-img").addEventListener("click",function(){
+    if(document.getElementById("carrousel").classList.contains("display-off"))
+    {
+      document.getElementById("carrousel").classList.remove("display-off");
+      document.getElementById("carrousel").classList.add("display-on");
+      document.getElementById("radio").classList.remove("display-on");
+      document.getElementById("radio").classList.add("display-off");
+    }
+  });
+// END Open event of main //===============
+//=================================================
+
+// Display deportes event =====================
+  document.getElementById("BtnDeportes").addEventListener("click",function(){
+    if(document.getElementById("clases-programas").classList.contains("display-off"))
+    {
+      document.getElementById("clases-programas").classList.remove("display-off");
+      document.getElementById("clases-programas").classList.add("display-on");
+    }
+    document.getElementById("deportes").classList.remove("display-off");
+    document.getElementById("deportes").classList.remove("display-on");
+    document.getElementById("deportes").classList.add("display-on");
+    document.getElementById("entretenimiento").classList.remove("display-on");
+    document.getElementById("entretenimiento").classList.remove("display-off");
+    document.getElementById("entretenimiento").classList.add("display-off");
+  });
+// END Display deportes event //===============
+//=================================================
+
+// Display deportes event =====================
+  document.getElementById("BtnCultura").addEventListener("click",function(){
+    if(document.getElementById("clases-programas").classList.contains("display-off"))
+    {
+      document.getElementById("clases-programas").classList.remove("display-off");
+      document.getElementById("clases-programas").classList.add("display-on");
+    }
+    document.getElementById("deportes").classList.remove("display-on");
+    document.getElementById("deportes").classList.remove("display-off");
+    document.getElementById("deportes").classList.add("display-off");
+    document.getElementById("entretenimiento").classList.remove("display-off");
+    document.getElementById("entretenimiento").classList.remove("display-on");
+    document.getElementById("entretenimiento").classList.add("display-on");
+  });
+// END Display deportes event //===============
