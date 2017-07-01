fb18a51706a4a7e667b26d4651c8ffb5d709562c
uboat46 <uboat46@hotmail.com>
2017-07-01 17:11:54 -0500

    added js files

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-var html = fs.readFileSync('dist/index.html');
-
-http.createServer(function (req, res) {
-    res.end(html);
-}).listen(8000);
